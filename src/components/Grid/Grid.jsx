2ffa4f9ed6ecb277c8fa0f46f13df6b724e67f5f
Jared Cox <coxj1@mcmaster.ca>
2021-07-12 21:10:47 -0400

    added clearing and showing no possible path

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const MAX_X = 20;
-const MAX_Y = 15;
+const MAX_X = 30;
+const MAX_Y = 17;
 
 const gridStyling = {
   width: `${MAX_X * 50}px`,
@@ -18,11 +18,12 @@ const rowStyling = {
 
 const elemStyling = (colorId) => {
   let colors = {
-    0: '#444444',
-    1: '#447777',
-    2: '#44AA77',
-    3: '#CC4444',
-    4: '#999933'
+    0: '#444444', //unvisited
+    1: '#447777', // visited
+    2: '#44AA77', //starting
+    3: '#eeee77', //ending
+    4: '#999933', //found path
+    5: '#aa3333' //no path found
   }
   return ({
     width: '40px',
@@ -66,18 +67,14 @@ class Grid extends React.Component {
       grid.push(row);
       prev.push(prevRow);
     }
-    // let startingPosition = [2, 3];
-    // let endingPosition = [5, 7];
-    // grid[startingPosition[0]][startingPosition[1]] = 2;
-    // grid[endingPosition[0]][endingPosition[1]] = 3;
 
     this.state = {
       grid,
       prev,
-      // startingPosition,
-      // endingPosition,
       clickType: 0,
-      queue: []
+      queue: [],
+      found: false,
+      setInterValID: 0
     };
   }
 
@@ -112,44 +109,61 @@ class Grid extends React.Component {
   }
 
   solve = () => {
-    setInterval(() => {
-      let { queue, grid, prev } = this.state;
-      let newQueue = [];
-      while (queue.length > 0) {
-        let current = queue.shift();
-        if (grid[current[0]][current[1]] == 3) {
-          queue = [];
-          newQueue = [];
+    let setInterValID =
+      setInterval(() => {
+        let { queue, grid, prev } = this.state;
+        let newQueue = [];
+        while (queue.length > 0) {
+          let current = queue.shift();
+          if (grid[current[0]][current[1]] == 3) {
+            queue = [];
+            newQueue = [];
 
-          let solutionChain = [];
-          let step = prev[current[0]][current[1]];
-          while (prev[step[0]][step[1]] != null) {
-            solutionChain.push(step);
-            grid[step[0]][step[1]] = 4;
-            step = prev[step[0]][step[1]];
-          }
-          this.setState({ solutionChain });
-        } else {
-          if (grid[current[0]][current[1]] == 0)
-            grid[current[0]][current[1]] = 1;
-
-          let neighbours = this.getUnvisitedNeigbours(current);
-          neighbours.forEach(neighbour => {
-            if (!queue.some(x => (
-              x[0] == neighbour[0] && x[1] == neighbour[1]
-            )) &&
-              (!newQueue.some(x => (
-                x[0] == neighbour[0] && x[1] == neighbour[1]
-              )))) {
-              prev[neighbour[0]][neighbour[1]] = [current[0], current[1]];
-              newQueue.push(neighbour);
+            let solutionChain = [];
+            let step = prev[current[0]][current[1]];
+            while (prev[step[0]][step[1]] != null) {
+              solutionChain.push(step);
+              grid[step[0]][step[1]] = 4;
+              step = prev[step[0]][step[1]];
             }
-          });
+            this.setState({ solutionChain, found: true });
+          } else {
+            if (grid[current[0]][current[1]] == 0)
+              grid[current[0]][current[1]] = 1;
+
+            let neighbours = this.getUnvisitedNeigbours(current);
+            neighbours.forEach(neighbour => {
+              if (!queue.some(x => (
+                x[0] == neighbour[0] && x[1] == neighbour[1]
+              )) &&
+                (!newQueue.some(x => (
+                  x[0] == neighbour[0] && x[1] == neighbour[1]
+                )))) {
+                prev[neighbour[0]][neighbour[1]] = [current[0], current[1]];
+                newQueue.push(neighbour);
+              }
+            });
+          }
+          if (newQueue.length == 0 && queue.length == 0 && !this.state.found) {
+            this.noPositionFound();
+          }
+        }
+        queue = newQueue;
+        this.setState({ queue, prev, grid })
+      }, 150);
+    this.setState({ setInterValID })
+  }
+
+  noPositionFound = () => {
+    let grid = this.state.grid;
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[0].length; j++) {
+        if (grid[i][j] == 1) {
+          grid[i][j] = 5;
         }
       }
-      queue = newQueue;
-      this.setState({ queue, prev, grid })
-    }, 150);
+    }
+    this.setState({ grid })
   }
 
   randomizeBlanks = () => {
@@ -162,6 +176,31 @@ class Grid extends React.Component {
       }
     }
     this.setState({ grid });
+  }
+
+  clearGrid = () => {
+    let grid = [];
+    let prev = [];
+    for (let i = 0; i < MAX_Y; i++) {
+      let row = [];
+      let prevRow = [];
+      for (let ii = 0; ii < MAX_X; ii++) {
+        row.push(0);
+        prevRow.push(null);
+      }
+      grid.push(row);
+      prev.push(prevRow);
+    }
+    clearInterval(this.state.setInterValID);
+
+    this.setState({
+      grid,
+      prev,
+      clickType: 0,
+      queue: [],
+      found: false,
+      setInterValID: 0
+    });
   }
 
   render() {
@@ -177,6 +216,7 @@ class Grid extends React.Component {
       ))}
       <button onClick={this.solve}>Click</button>
       <button onClick={this.randomizeBlanks}>Random</button>
+      <button onClick={this.clearGrid}>Clear</button>
     </div>)
   }
 }
